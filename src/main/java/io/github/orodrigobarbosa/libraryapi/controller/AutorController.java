91dd9c13f98895bad7d6f2ccecc4059dcafef44e
Rodrigo Barbosa <rodbarbosa.dv@gmail.com>
2025-04-18 20:35:07 -0300

    Buscar Por Id e Deletar Por Id

--- src/main/java/io/github/orodrigobarbosa/libraryapi/controller/AutorController.java
+++ src/main/java/io/github/orodrigobarbosa/libraryapi/controller/AutorController.java
@@ -1,16 +1,16 @@
 package io.github.orodrigobarbosa.libraryapi.controller;
 
 import io.github.orodrigobarbosa.libraryapi.controller.dto.AutorDTO;
+import io.github.orodrigobarbosa.libraryapi.model.Autor;
 import io.github.orodrigobarbosa.libraryapi.service.AutorService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Optional;
+import java.util.UUID;
 
 @AllArgsConstructor
 @RestController
@@ -26,9 +26,33 @@ public class AutorController {
         autorService.salvarAutor(autorEntidade);
 
         //hhttp://localhost:8080/autores/id
-       URI location =  ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(autorEntidade.getId()).toUri();
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(autorEntidade.getId()).toUri();
         return ResponseEntity.created(location).build();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<AutorDTO> buscarAutorPorId(@PathVariable("id") String id) {
+        var idAutor = UUID.fromString(id);
+        Optional<Autor> autorOptional = autorService.buscarPorId(idAutor);
+        if(autorOptional.isPresent()) {
+            Autor autor = autorOptional.get();
+            AutorDTO dto = new AutorDTO(autor.getId(), autor.getNome(), autor.getDataNascimento(), autor.getNacionalidade());
+            return ResponseEntity.ok(dto);
+        }
+        return ResponseEntity.notFound().build();
 
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarAutorPorId(@PathVariable("id") String id) {
+        var idAutor = UUID.fromString(id);
+        Optional<Autor> autorOptional = autorService.buscarPorId(idAutor);
+
+
+        if(autorOptional.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        autorService.deletar(autorOptional.get());
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/io/github/orodrigobarbosa/libraryapi/service/AutorService.java
+++ src/main/java/io/github/orodrigobarbosa/libraryapi/service/AutorService.java
@@ -5,6 +5,9 @@ import io.github.orodrigobarbosa.libraryapi.repository.AutorRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @AllArgsConstructor
 @Service
 public class AutorService {
@@ -14,5 +17,14 @@ public class AutorService {
     public Autor salvarAutor(Autor autor) {
         return autorRepository.save(autor);
 
+    }
+
+
+    public Optional<Autor> buscarPorId(UUID id) {
+        return autorRepository.findById(id);
+    }
+
+    public void deletar(Autor autor) {
+        autorRepository.delete(autor);
     }
 }
