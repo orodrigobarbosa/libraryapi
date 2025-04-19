105621b149e0ecf619b6f9ddbb8107fdc9970d92
Rodrigo Barbosa <rodbarbosa.dv@gmail.com>
2025-04-19 12:29:12 -0300

    pesquisa com filtro

--- src/main/java/io/github/orodrigobarbosa/libraryapi/repository/AutorRepository.java
+++ src/main/java/io/github/orodrigobarbosa/libraryapi/repository/AutorRepository.java
@@ -4,8 +4,15 @@ import io.github.orodrigobarbosa.libraryapi.model.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface AutorRepository extends JpaRepository<Autor, UUID> {
+
+    List<Autor> findByNome(String nome);
+
+    List<Autor> findByNacionalidade(String nacionalidade);
+
+    List<Autor> findByNomeAndNacionalidade(String nome, String nacionalidade);
 }
